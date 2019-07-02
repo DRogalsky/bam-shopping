1c95016c16db56ebd01e5d85b22ab7b69ef97705
dnl092 <drogalsky14@my.whitworth.edu>
2019-07-01 19:54:11 -0700

    added data to the database

---
+++ SQL/seeds.sql
@@ -0,0 +1,31 @@
+USE bamazon_db;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Loveseat', 'Furniture', 299.97, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Chair', 'Furniture', 49.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Table', 'Furniture', 120.00, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Queen Bed', 'Furniture', 599.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('VR Headset', 'Gaming', 499.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Mad Catz model Player 2', 'Gaming', 9.99, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Guilty Gear Xrd Revelator Rev 2 + all DLC', 'Gaming', 59.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Under Night In-Birth Exe:Late[st]', 'Gaming', 29.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Tekken 7', 'Gaming', 59.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Azathoth', 'Elder Gods', 20.00, 1);
